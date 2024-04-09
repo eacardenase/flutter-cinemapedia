e7457393a9431fc9f8d3e743a81abb072e6a669d
Edwin Cardenas <eacardenase@gmail.com>
2024-04-08 19:49:12 -0500

    feat: add movies providers

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({
+    required this.fetchMoreMovies,
+  }) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage++);
+
+    state = [
+      ...state,
+      ...movies,
+    ];
+  }
+}
+
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final getNowPlayingMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+
+  return MoviesNotifier(
+    fetchMoreMovies: getNowPlayingMovies,
+  );
+});
