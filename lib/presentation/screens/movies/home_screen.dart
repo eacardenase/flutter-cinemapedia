62b4db6662bada02b014c806adf465a5d6e06415
Edwin Cardenas <eacardenase@gmail.com>
2024-04-09 08:45:42 -0500

    feat: add custom appbar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -42,17 +44,24 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       );
     }
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
+    return Column(
+      children: [
+        const CustomAppBar(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context, index) {
+              final movie = nowPlayingMovies[index];
 
-        return ListTile(
-          title: Text(
-            movie.title,
+              return ListTile(
+                title: Text(
+                  movie.title,
+                ),
+              );
+            },
           ),
-        );
-      },
+        )
+      ],
     );
   }
 }
