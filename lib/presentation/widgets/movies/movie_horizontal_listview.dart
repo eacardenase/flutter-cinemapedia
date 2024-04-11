e5dcbd00bca14ad9cc5195a86f163be95e099df3
Edwin Cardenas <eacardenase@gmail.com>
2024-04-11 16:04:24 -0500

    feat: add movies cards

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:animate_do/animate_do.dart';
+
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MovieHorizontalListView extends StatelessWidget {
@@ -19,14 +21,22 @@ class MovieHorizontalListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 150,
+      height: 350,
       child: Column(
         children: [
           if (title != null && subtitle != null)
             _Title(
               title: title!,
               subtitle: subtitle!,
-            )
+            ),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              itemCount: movies.length,
+              itemBuilder: (context, index) => _Slide(movie: movies[index]),
+            ),
+          )
         ],
       ),
     );
@@ -45,7 +55,6 @@ class _Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final titleStyle = Theme.of(context).textTheme.titleLarge;
-    final subtitleStyle = Theme.of(context).textTheme.titleMedium;
 
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 10),
@@ -62,9 +71,99 @@ class _Title extends StatelessWidget {
             onPressed: () {},
             child: Text(
               subtitle,
-              style: subtitleStyle,
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  const _Slide({
+    required this.movie,
+  });
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
+    return Container(
+      padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: SizedBox(
+              width: 150,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+                child: Image.network(
+                  movie.posterPath,
+                  width: 150,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress != null) {
+                      return Container(
+                        color: Colors.black12,
+                        alignment: Alignment.center,
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+
+                    return FadeIn(
+                      child: child,
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 3),
+
+          // Title
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 1,
+              style: textStyles.titleSmall,
+              // textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+
+          // Rating
+          Row(
+            children: [
+              Icon(
+                Icons.star_half_outlined,
+                color: Colors.yellow.shade800,
+              ),
+              const SizedBox(width: 3),
+              Text(
+                movie.voteAverage.toStringAsFixed(1),
+                style: textStyles.bodyMedium
+                    ?.copyWith(color: Colors.yellow.shade800),
+              ),
+              const SizedBox(width: 3),
+              Text(
+                movie.popularity.toString(),
+                style: textStyles.bodyMedium,
+              )
+            ],
+          )
         ],
       ),
     );
