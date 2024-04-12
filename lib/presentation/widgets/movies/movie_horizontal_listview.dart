ec712f9cdfbd807ee21bf5434ed8fa77d924a690
Edwin Cardenas <eacardenase@gmail.com>
2024-04-12 13:00:48 -0500

    bugfix: prevent unnecessary movie fetching on infinite scroll

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -36,8 +36,6 @@ class _MovieHorizontalListViewState extends State<MovieHorizontalListView> {
 
       if ((scrollController.position.pixels + 200) >=
           scrollController.position.maxScrollExtent) {
-        print('Load next page');
-
         widget.loadNextPage!();
       }
     });
