1b033a111b9ee529cd583b645666fd0e061cc833
Edwin Cardenas <eacardenase@gmail.com>
2024-04-17 21:25:30 -0500

    feat: add movie overview on search suggestions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -55,17 +55,92 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
         return ListView.builder(
           itemCount: movies.length,
-          itemBuilder: (context, index) {
-            final movie = movies[index];
-
-            return ListTile(
-              title: Text(
-                movie.title,
-              ),
-            );
-          },
+          itemBuilder: (context, index) => _MovieSearchItem(
+            movie: movies[index],
+          ),
         );
       },
+    );
+  }
+}
+
+class _MovieSearchItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieSearchItem({
+    required this.movie,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textTheme = Theme.of(context).textTheme;
+
+    return SizedBox(
+      height: 150,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Image
+            SizedBox(
+              height: double.infinity,
+              width: size.width * 0.25,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) => FadeIn(
+                    child: child,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            // Movie Desc
+            SizedBox(
+              width: size.width * 0.65,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    movie.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: textTheme.titleMedium,
+                  ),
+                  Text(
+                    movie.overview,
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                    style: textTheme.bodyMedium,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_half_outlined,
+                        color: Colors.yellow.shade800,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        movie.voteAverage.toStringAsFixed(1),
+                        style: textTheme.bodyMedium?.copyWith(
+                          color: Colors.yellow.shade900,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
