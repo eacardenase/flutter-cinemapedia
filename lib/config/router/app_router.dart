848d8edf85f4ee47c0cd655e5c18da99d451db9c
Edwin Cardenas <eacardenase@gmail.com>
2024-04-22 14:53:51 -0500

    feat: hide navigation bar on movie details screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,16 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:cinemapedia/presentation/views/views.dart';
 
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+final GlobalKey<NavigatorState> _movieDetailsNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'movie_details');
+
 final appRouter = GoRouter(
   initialLocation: '/',
+  navigatorKey: _rootNavigatorKey,
   routes: [
     StatefulShellRoute.indexedStack(
       builder: (context, state, navigationShell) =>
           HomeScreen(childView: navigationShell),
       branches: [
         StatefulShellBranch(
+          navigatorKey: _movieDetailsNavigatorKey,
           routes: [
             GoRoute(
               path: '/',
@@ -19,6 +27,7 @@ final appRouter = GoRouter(
                 GoRoute(
                   path: 'movie/:id',
                   name: MovieScreen.name,
+                  parentNavigatorKey: _rootNavigatorKey,
                   builder: (context, state) {
                     final movieId = state.pathParameters['id']!;
 
