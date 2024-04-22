ba13c141e916cb4b364939003d63d9f23bdb3a58
Edwin Cardenas <eacardenase@gmail.com>
2024-04-21 21:15:28 -0500

    feat: implement navigation between views

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,26 +1,77 @@
 import 'package:flutter/material.dart';
 
+import 'package:go_router/go_router.dart';
+
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.path;
+
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
+    return NavigationBar(
       elevation: 0,
-      items: const [
-        BottomNavigationBarItem(
+      selectedIndex: getCurrentIndex(context),
+      onDestinationSelected: (index) => _onItemTapped(context, index),
+      indicatorColor: Colors.transparent,
+      destinations: const [
+        NavigationDestination(
           label: 'Home',
-          icon: Icon(Icons.home),
+          icon: _NavigationIcon(Icons.home_outlined),
+          selectedIcon: _NavigationIcon(Icons.home),
         ),
-        BottomNavigationBarItem(
+        NavigationDestination(
           label: 'Categories',
-          icon: Icon(Icons.label_outline),
+          icon: _NavigationIcon(Icons.label_outline),
+          selectedIcon: _NavigationIcon(Icons.label),
         ),
-        BottomNavigationBarItem(
+        NavigationDestination(
           label: 'Favorites',
-          icon: Icon(Icons.favorite_outline),
+          icon: _NavigationIcon(Icons.favorite_outline),
+          selectedIcon: _NavigationIcon(Icons.favorite),
         ),
       ],
+    );
+  }
+}
+
+class _NavigationIcon extends StatelessWidget {
+  final IconData icon;
+
+  const _NavigationIcon(this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      icon,
+      color: Theme.of(context).colorScheme.primary,
     );
   }
 }
