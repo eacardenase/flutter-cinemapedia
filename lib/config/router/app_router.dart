d89fde562a335568fc9042f580e79f638440366b
Edwin Cardenas <eacardenase@gmail.com>
2024-04-21 20:29:54 -0500

    feat: shell routes setup

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -6,25 +6,29 @@ import 'package:cinemapedia/presentation/views/views.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(
-        childView: FavoriteView(),
-      ),
+    ShellRoute(
+      builder: (context, state, child) => HomeScreen(childView: child),
       routes: [
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
-          builder: (context, state) {
-            final movieId = state.pathParameters['id'];
+          path: '/',
+          builder: (context, state) => const HomeView(),
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id']!;
 
-            return MovieScreen(
-              movieId: movieId!,
-            );
-          },
+                return MovieScreen(movieId: movieId);
+              },
+            )
+          ],
         ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) => const FavoriteView(),
+        )
       ],
-    ),
+    )
   ],
 );
