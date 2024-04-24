b893d9b83edfdce6423827e0122d890f2142e6e9
Edwin Cardenas <eacardenase@gmail.com>
2024-04-23 19:20:28 -0500

    feat: isardb setup

--- lib/infrastructure/datasources/isar_database_datasource.dart
+++ lib/infrastructure/datasources/isar_database_datasource.dart
@@ -1,9 +1,23 @@
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:cinemapedia/domain/datasources/local_database_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class IsarDatabaseDatasource extends LocalDatabaseDatasource {
+  late Future<Isar> db;
+
+  IsarDatabaseDatasource() {
+    db = _openDb();
+  }
+
   @override
   Future<bool> isFavorite(int movieId) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
     throw UnimplementedError();
   }
 
@@ -12,8 +26,17 @@ class IsarDatabaseDatasource extends LocalDatabaseDatasource {
     throw UnimplementedError();
   }
 
-  @override
-  Future<void> toggleFavorite(Movie movie) {
-    throw UnimplementedError();
+  Future<Isar> _openDb() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [MovieSchema],
+        directory: dir.path,
+        inspector: true,
+      );
+    }
+
+    return Future.value(Isar.getInstance());
   }
 }
